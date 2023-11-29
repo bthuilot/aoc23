6301da0d05f2612384a112c5e5c15815a4fd6b3c
Bryce Thuilot <bryce@thuilot.io>
2023-11-29 18:56:07 -0500

    fix: arg parsing
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- bin/main.ml
+++ bin/main.ml
@@ -2,19 +2,22 @@ open Advent_of_code_2023
 
 let usage_msg = "aoc23 <day> [<additional days>] ..."
 
-let days_to_run = ref []
+module SI = Set.Make(Int)
 
-let anon_fun filename = days_to_run := filename :: !days_to_run
+let days_to_run = ref SI.empty
 
-let itoa = Printf.sprintf "%d"
+let anon_fun arg =
+  match int_of_string_opt arg with
+  | None -> ()
+  | Some day -> days_to_run := SI.add day !days_to_run
+
 
 let () =
   Arg.parse [] anon_fun usage_msg;
-  
   if
-    List.compare_length_with !days_to_run 0 = 0
+    SI.is_empty !days_to_run
   then
-    days_to_run := List.init 25 (fun x -> itoa (x + 1));
+    days_to_run := List.init 25 ((+) 1) |> SI.of_list;
   
   print_endline "";
   print_endline "##########################";
@@ -23,7 +26,7 @@ let () =
   print_endline "";
   Calendar.days |> List.iteri (fun num d ->
                   if
-                    List.exists ((=) (itoa num)) !days_to_run
+                    SI.mem num !days_to_run
                   then (d#run; print_endline "")
                 )
   
