88e598506a897a37538108b5a2397ec722a6ab6d
Bryce Thuilot <bthuilot@gmail.com>
2023-12-02 09:32:06 -0500

    feat: complete day 2 (#5)
    * feat: complete day 2
    
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>
    
    ---------
    
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- test/aoc23_test.ml
+++ test/aoc23_test.ml
@@ -1,5 +1,6 @@
 let () =
   Alcotest.run "Advent of Code 2023" [
       "Day 0", Day00_test.suite;
-      "Day 1", Day01_test.suite
+      "Day 1", Day01_test.suite;
+      "Day 2", Day02_test.suite
     ]
