d9c0d900b10f6cee56aa349879046c4904bc4bb6
Bryce Thuilot <bryce@thuilot.io>
2023-12-02 09:19:36 -0500

    feat: complete day 2
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- lib/days/day02.ml
+++ lib/days/day02.ml
@@ -1,8 +1,114 @@
+
+(** Advent of Code - Day 2
+
+    https://adventofcode.com/2020/day/2
+
+    This day involves parsing game results and calculating
+    first the game IDs of possible games, and then the
+    "power" of each game, which is the product of the
+    minimum number required of each color cube used in the game.
+
+    I spent a lot of time on the parsing of the game results
+    into a common data structure, which resulted in the
+    individual parts being fairly simple.
+
+    The first part is a simple filter of the possible games
+    based on the maximum number of cubes of each color. If any
+    game has more than the maximum, it is not possible.
+
+    The second part is a simple fold over the possible games
+    to find the maximum number of cubes of each color used in
+    the game, and then multiplying them together.
+ *)
+
+(** [cube_reveal] represents the number of cubes of each color
+    revealed in a single turn of a game. *)
+type cube_reveal = {
+    red : int;
+    green : int;
+    blue : int;
+  }
+
+(** [game] represents a single game, with a unique ID and a list
+    of [cube_reveal]s. *)
+type game = {
+    id : int;
+    reveals: cube_reveal list;
+  }
+
+(** [parse_gameid line] parses the game ID from a line of input. *)
+let parse_gameid (line : string) : int =
+  let r = Str.regexp {|Game \([0-9]+\):.+|} in
+  let _ = Str.string_partial_match r line 0 in
+  let game_id = Str.matched_group 1 line in
+  int_of_string game_id
+
+
+(** [parse_reveals line] parses the list of [cube_reveal]s from a line of input. *)
+let parse_reveals (line : string) : cube_reveal list =
+  let reveals = match String.split_on_char ':' line with
+    | [_; reveals] -> reveals |> String.split_on_char ';'
+    | _ -> [] in
+  List.map (fun reveal ->
+      let colors = reveal |> String.split_on_char ',' in
+      List.fold_left (fun acc colorS ->
+          let color = String.trim colorS in
+          let r = Str.regexp {|\([0-9]+\) \([a-z]+\)|} in
+          let _ = Str.string_match r color 0 in
+          let amt = Str.matched_group 1 color in
+          let color = Str.matched_group 2 color in
+          match color with
+          | "red" -> { acc with red = int_of_string amt }
+          | "green" -> { acc with green = int_of_string amt }
+          | "blue" -> { acc with blue = int_of_string amt }
+          | _ -> acc
+        ) { red = 0; green = 0; blue = 0 } colors
+    ) reveals
+
+
+(** [parse_input input] parses the input into a list of [game]s. *)
+let parse_input (input : string) : game list =
+  let lines = String.split_on_char '\n' input in
+  List.map (fun line ->
+      let game_id = parse_gameid line in
+      let reveals = parse_reveals line in
+      { id = game_id; reveals = reveals }
+    ) lines
+
+(** [find_possible_games gs] finds the possible games from a list of [game]s. *)
+let find_possible_games (gs : game list) =
+  let maxRed, maxBlue, maxGreen = 12, 14, 13 in
+  List.filter (fun g ->
+      List.exists (fun r -> r.red > maxRed || r.green > maxGreen || r.blue > maxBlue) g.reveals |> not
+    ) gs
+
+
+(** [find_game_powers gs] finds the "power" of each game from a list of [game]s. *)
+let find_game_powers (gs : game list) =
+  List.map (fun g ->
+      let (minR, minG, minB) =
+        List.fold_left(fun (minR, minG, minB) r ->
+            let red = max r.red minR in
+            let green = max r.green minG in
+            let blue = max r.blue minB in
+            (red, green, blue)
+          ) (0, 0, 0) g.reveals in
+      minR * minG * minB
+    ) gs
+
 class t =
   object (_)
     inherit Day_intf.t 2
-    method part1 (_ : string) : string = "Not Implemented"
-    method part2 (_ : string) : string = "Not Implemented"
+
+    (** [part1] finds the sum of the IDs of the possible games. *)
+    method part1 (i : string) : string =
+      let gs = parse_input i in
+      find_possible_games gs |> List.fold_left (fun acc g -> acc + g.id) 0 |> string_of_int
+
+    (** [part2] finds the sum of the "power" of each game. *)
+    method part2 (i : string) : string =
+      let gs = parse_input i in
+        find_game_powers gs |> List.fold_left (+) 0 |> string_of_int
   end
 
 
---
+++ test/days/day02_test.ml
@@ -0,0 +1,29 @@
+open Alcotest
+
+let day = new Advent_of_code_2023.Day02.t
+
+let input = {|Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green|}
+
+let test_part1 () =
+  let expected = "8" in
+  let actual = day#part1 input  in
+  check string "part1 is valid" expected actual
+
+
+let test_part2 () =
+  let expected = "2286" in
+  let actual = day#part2 input in
+  check string "part2 is valid" expected actual
+
+ 
+let suite =
+  [
+    "part1", `Quick, test_part1;
+    "part2", `Quick, test_part2;
+  ]
+
+
