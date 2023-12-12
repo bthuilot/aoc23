4844f218be1eb080d9b834b579ae0f893076ff52
Bryce Thuilot <bryce@thuilot.io>
2023-12-11 19:52:35 -0500

    add skeleton
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

---
+++ test/days/day12_test.ml
@@ -0,0 +1,25 @@
+open Alcotest
+
+let day = new Advent_of_code_2023.Day12.t
+
+let i = {|test|}
+
+let test_part1 () =
+  let expected = "test" in
+  let actual = day#part1 i in
+  check string "part1 is valid" expected actual
+
+
+let test_part2 () =
+  let expected = "TEST" in
+  let actual = day#part2 i in
+  check string "part2 is valid" expected actual
+
+ 
+let suite =
+  [
+    "part1", `Quick, test_part1;
+    "part2", `Quick, test_part2;
+  ]
+
+
